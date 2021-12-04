9a5977ebfacc9a809874692f01a5ae1cb38ab36b
HaydenSolo <haydensolo@gmail.com>
2021-12-04 23:15:42 +1300

    Day 4 completed

--- day2/d2.jl
+++ day2/d2.jl
@@ -1,20 +1,24 @@
-data = split.(readlines("input.txt"))
-list = [(dat[1], parse(Int, dat[2])) for dat in data]
-horizontal = 0
-vertical = 0
-aim = 0
-for command in list
-	num = command[2]
-	text = command[1]
-	if text == "forward"
-		global horizontal += num
-		global vertical += aim*num
-	elseif text == "down"
-		global aim += num
-	elseif text == "up"
-		global aim -= num
+const data = split.(readlines("input.txt"))
+const list = [(dat[1], parse(Int, dat[2])) for dat in data]
+
+function locate(list)
+	horizontal = 0
+	vertical = 0
+	aim = 0
+	for command in list
+		num = command[2]
+		text = command[1]
+		if text == "forward"
+			horizontal += num
+			vertical += aim*num
+		elseif text == "down"
+			aim += num
+		elseif text == "up"
+			aim -= num
+		end
 	end
+	return horizontal*vertical
 end
-println(horizontal)
-println(vertical)
-print(horizontal*vertical)
+
+x = locate(list)
+print(x)

ca97cdf880ed805f80f2ee0b23caeb4f7540815e
HaydenSolo <haydensolo@gmail.com>
2021-12-05 23:45:25 +1300

    Day 5

---
+++ day4/d42.jl
@@ -0,0 +1,61 @@
+data = readlines("input.txt")
+const nums = parse.(Int, split(popfirst!(data), ','))
+data = filter(n -> n != "", data)
+
+data = map(line -> parse.(Int, line), split.(data))
+
+Square = Union(Int, Nothing)
+
+a = convert(Union, 5)
+
+masknums(board, num) = map(line -> map(digit -> digit == num ? -1 : digit, line), board)
+
+function checkwin(board)
+	for i in 1:5
+		verticalwin = true
+		horizontalwin = true
+		for j in 1:5
+			board[i][j] == -1 || (verticalwin = false)
+			board[j][i] == -1 || (horizontalwin = false)
+		end
+		(verticalwin || horizontalwin) && (return true)
+	end
+	return false
+end
+
+function sumboard(board)
+	ret = 0
+	for row in board, num in row
+		ret += num == -1 ? 0 : num
+	end
+	return ret
+end
+
+function reportwin(board, num)
+	println(board)
+	println(sumboard(board))
+	println(num)
+	println(sumboard(board)*num)
+end
+
+function run1(data, nums)
+	boards = []
+	num_boards = length(data)/5
+	for board in 1:5:length(data)
+		push!(boards, data[board:board+4])
+	end
+	for num in nums
+		maskedboards = []
+		for board in boards
+			newboard = masknums(board, num)
+			push!(maskedboards, newboard)
+			if checkwin(newboard)
+				reportwin(newboard, num)
+				return
+			end
+		end
+		boards = maskedboards
+	end
+end
+
+run1(data, nums)
---
+++ day5/d5.jl
@@ -0,0 +1,66 @@
+const data = split.(readlines("input.txt"), " -> ")
+full = map(line -> map(str -> split(str, ","), line), data)
+
+struct Point
+	x::Integer
+	y::Integer
+	Point(a::Vector{SubString{String}}) = new(parse(Int, a[1]), parse(Int, a[2]))
+	Point(x::Integer, y::Integer) = new(x, y)
+end
+
+struct Line
+	start::Point
+	finish::Point
+	function Line(line::Vector{Point}) 
+		a = line[1]
+		b = line[2]
+		(b.x > a.x || b.y > a.y) ? new(a, b) : new(b, a)
+	end
+end
+
+checkline(line) = return line[1][1] == line[2][1] || line[1][2] == line[2][2]
+
+# full = filter(checkline, full)
+full = map(line -> Line(map(a -> Point(a), line)), full)
+
+allpoints = []
+
+println(full[1])
+visited = Set{Point}()
+visitedcounted = Set{Point}()
+doubles = 0
+
+function getfull(line::Line)
+	if line.start.x == line.finish.x || line.start.y == line.finish.y
+		full = Vector{Point}()
+		for x in line.start.x:line.finish.x, y in line.start.y:line.finish.y
+			point = Point(x, y)
+			push!(full, point)
+		end
+		return full
+	end
+
+	xdistance = line.finish.x - line.start.x
+	ydistance = line.finish.y - line.start.y
+
+	xstep = line.finish.x > line.start.x ? 1 : -1
+	ystep = line.finish.y > line.start.y ? 1 : -1
+
+	allfull = [Point(x, y) for x in line.start.x:xstep:line.finish.x, y in line.start.y:ystep:line.finish.y]
+	full = [allfull[i, i] for i in 1:size(allfull)[1]]
+	return full
+end
+
+
+for line in full
+	points = getfull(line)
+	for point in points
+		if point in visited
+			point in visitedcounted || (global doubles += 1; push!(visitedcounted, point))
+		else
+			push!(visited, point)
+		end
+	end
+end
+
+println(doubles)
